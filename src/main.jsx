a0b35355d1043359b4da69068ec485ce43ab77de
Awaneesh075 <chaturvedianhishek9554@gmail.com>
2025-11-10 16:27:25 +0530

    fix file

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import TodoApp from './Todo.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <TodoApp/>
   </StrictMode>,
 )
